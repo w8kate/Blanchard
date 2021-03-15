a1b8987d0173f49d8b8a0deca0456e506d6dd0a9
Kate Kusheleva <kushelevaekaterina@gmail.com>
2021-03-15 15:41:54 +0400

    catalog fixed

--- js/catalog.js
+++ js/catalog.js
@@ -96,3 +96,21 @@
 
 })();
 
+// closing search when changing screen width
+function resizeWidthOnly(a,b) {
+  var c = [window.innerWidth];
+  return onresize = function() {
+    var d = window.innerWidth,
+        e = c.length;
+    c.push(d);
+    if(c[e]!==c[e-1]){
+      clearTimeout(b);
+      b = setTimeout(a, 50);
+    }
+  }, a;
+}
+
+resizeWidthOnly(function() {
+  location.reload();
+});
+
